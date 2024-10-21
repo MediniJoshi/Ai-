3066bb3f8d4f9590e8ce17368ce6b4665c3df545
MediniJoshi <122529168+MediniJoshi@users.noreply.github.com>
2024-10-21 18:09:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Website is live at https://ai-mock-interview-xi.vercel.app/  , use laptop for better experience
---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://AI-MOCK_INTERVIEWER_owner:JnGq0W8mDFha@ep-little-block-a55hfvfb.us-east-2.aws.neon.tech/AI-MOCK_INTERVIEWER?sslmode=require' ,
+    }
+  };
